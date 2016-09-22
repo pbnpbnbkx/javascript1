4db0ff673b853f234737cc61b46ed45f1ceb9dac
Kevin <kevinjawyeang@gmail.com>
2016-09-22 11:18:26 +0800

    test1
    test1 description

---
+++ 0sample1/JavaScript1.js
@@ -0,0 +1,77 @@
+﻿var flicker = {
+    flickerStop: false,
+    /*unicode : '!\'#$%'()*+,-./0123456789:;?@`aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ{[|\}]~^_',*/
+    unicode: '!?$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    getRandomInt: function (min, max) {
+        // console.log(min + ' ' + max);
+        // console.log('weee');
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    randomCharacter: function () {
+        return this.unicode[Math.floor(Math.random() * this.unicode.length)];
+    },
+    replaceAt: function (text, character, index) {
+        return text.substr(0, index) + character + text.substr(index + character.length);
+    },
+    init: function (element, min, max, delay) {
+        var elements = document.querySelectorAll(element);
+        for (let i = 0; i < elements.length; i++) {
+            var el = elements[i];
+
+            var str = el.innerText.trim(),
+                bank = [],
+                newStr = '';
+
+            for (let j = 0; j < str.length; j++) {
+                bank[j] = this.getRandomInt(min, max);
+            }
+
+            this.mix(el, str, newStr, bank, delay);
+        }
+
+    },
+    mix: function (el, str, newStr, bank, delay) {
+        var i = 0;
+        var storeArr = [];
+        while (str !== newStr) {
+            if (str[i] !== newStr[i]) {
+                if (bank[i] !== 0) {
+                    if (str[i] !== ' ') {
+                        newStr = this.replaceAt(newStr, this.randomCharacter(), i);
+                    }
+                    else {
+                        newStr = this.replaceAt(newStr, ' ', i);
+                    }
+                    bank[i]--;
+                } else {
+                    newStr = this.replaceAt(newStr, str[i], i);
+                }
+
+                storeArr.push(newStr);
+            }
+            else {
+                i++;
+            }
+
+            if (str === newStr) {
+                this.flickerStop = true;
+                this.finalResult(storeArr, el, delay);
+                break;
+            }
+        }
+    },
+    finalResult: function (storeArr, el, delay) {
+        var n = 0;
+        var time = setInterval(() => {
+            el.innerText = storeArr[n];
+        n++;
+
+        if (n === storeArr.length) {
+            clearInterval(time);
+        }
+    }, delay);
+}
+};
+
+// 破碎字體特效
+flicker.init('.flicker', 1, 10, 40);
---
+++ 0sample1/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="javascript1._0sample1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <script src="../Scripts/jquery-1.12.4.min.js"></script>
+    <style>
+        body {
+            background-color: #efefef;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 class="flicker">Jason Tseng</h1>
+
+            <p class="flicker">
+                Flicker Text Without jQuery and shows one by one.
+            </p>
+        </div>
+    </form>
+</body>
+<script src="JavaScript1.js"></script>
+</html>
